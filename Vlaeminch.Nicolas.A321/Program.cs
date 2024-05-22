3d0278eedf7d00d835134557c5af2d0977b6f3d7
Nicolas Vlaeminch <nicolasvlaeminch777@gmail.com>
2024-05-22 18:03:26 -0300

    Agregar archivos de proyecto.

---
+++ Vlaeminch.Nicolas.A321/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vlaeminch.Nicolas.A321
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba");
+            Console.ReadLine();
+        }
+    }
+}
